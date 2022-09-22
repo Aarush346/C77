ec7203d6c788d7d1e318cf62279478e20fceebfd
Aarush346 <94644920+Aarush346@users.noreply.github.com>
2022-09-22 21:01:41 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name="Aarush";
+var last_name="Bohra";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=3;
+var add=a + b;
+console.log(add);
+var multiply=a*b;
+console.log(multiply);
+var subtraction=a - b;
+console.log(subtraction);
+
